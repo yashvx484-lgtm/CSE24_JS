deebc24bb0d05359ac8315920771d54c78b78218
Yash Vaish <yashvx484@gmail.com>
2026-03-23 20:31:09 +0530

    Adding 2nd folder

---
+++ 2/scipt.js
@@ -0,0 +1,56 @@
+var a = 100; 
+var b = 13;                                   
+var c = 10;           
+var linebreak = "<br>";
+console.log("a + b + c = ");
+result = a + b + c;
+console.log(result);
+console.log(linebreak);
+console.log("a + b = ");
+result = a + b;
+console.log(result);
+console.log(linebreak);
+console.log("a - b = ");
+result = a - b;
+console.log(result);
+console.log(linebreak);
+console.log("a / b = ");
+result = a / b;
+console.log(result);
+console.log(linebreak);
+console.log("a % b = ");
+result = a % b;
+console.log(result);
+console.log
+
+//arithmetic operators
+
+var a = 10;
+var b = 20;
+var linebreak = "<br />";
+console.log("(a == b) => ");
+result = (a == b);
+console.log(result);
+console.log(linebreak);
+console.log("(a < b) => ");
+result = (a < b);
+console.log(result);
+console.log(linebreak);
+dconsole.log("(a > b) => ");
+result = (a > b);
+console.log(result);
+console.log(linebreak);
+console.log("(a != b) => ");
+result = (a != b);
+console.log(result);
+console.log(linebreak);
+console.log("(a >= b) => ");
+result = (a >= b);
+console.log(result);
+console.log(linebreak);
+console.log("(a <= b) => ");
+result = (a <= b);
+console.log(result);
+console.log(linebreak);
+
+//comparison operators
