428c7131567ab901466fa3ef955f6611914760f9
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:39:34 +0530

    MethodChaining Method in JS

---
+++ Practice/Methods/methodchaining.js
@@ -0,0 +1,6 @@
+console.log("Method Chaining:- It is a technique in which multiple methods are called on the same object in a single line of code, allowing for a more concise and readable code.");
+let name2="  yash vaish  ";
+console.log(name2);
+//using method chaininng to easily do two functions at once/
+let mcupdate=name2.trim().toLocaleUpperCase();
+console.log(mcupdate);
