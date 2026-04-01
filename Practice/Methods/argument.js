fda723a4edf3de3a83e2a39667ac78f3f0f99a37
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:38:23 +0530

    argument Method in JS

---
+++ Practice/Methods/argument.js
@@ -0,0 +1,8 @@
+ console.log("indexOf() METHOD'S:- returns the index of the first occurrence of a specified value in a string");
+ 
+ let msg2="ilovecodingi";
+    console.log(msg2.indexOf("love"));
+    console.log(msg2.indexOf("coding"));
+    console.log(msg2.indexOf("o"));
+    console.log(msg2.indexOf("a"));
+      
