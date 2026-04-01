d2ec26fbf6bfc77e8656a74aa65451ab6e6d5866
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:27:31 +0530

    problem 5

---
+++ Practice/prompt.js
@@ -0,0 +1,12 @@
+let vae= prompt("enter your mobile number only if you like me :)");
+console.log(vae);
+let var1="Please";
+let var3="I know you!!";
+let var4=prompt(" Listen "+var1+" "+var3+ " dont so give your friends number intead :) ");
+console.log(var4);
+//mE:- why it is showing error?
+//  AI:- Because prompt is not defined in node js,
+//  it is only defined in browser.
+//   So we have to run this code 
+//   in browser.
+  //mE:-ok :)
