2c854e02abe2d5c26a4ea7b9eb948a2ffeb2029b
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:25:20 +0530

    problem 2

---
+++ Practice/alert.js
@@ -0,0 +1,7 @@
+let alert1= alert("I think you are spying MEE!! hmm?");
+console.log(alert1);
+alert("Stop it right nowww!!");
+
+let a1="WHAT??";
+let a2="I CAN";
+alert( "You know " + a1 + a2 +" DO SMJHAA?");
---
+++ Practice/warning&error.js
@@ -0,0 +1,4 @@
+let yash= "yash";
+console.error(yash);
+console.warn(yash);
+    ``
