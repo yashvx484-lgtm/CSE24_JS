8a441d008f5c5338316ec909a62849f493840ca8
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:36:06 +0530

    toUpperLower Method Strings in JS

---
+++ Practice/Methods/toUpperLowerCases.js
@@ -0,0 +1,9 @@
+console.log("toUpper&LowerCase() METHOD'S:- changes the case of the string");
+let name= "yash vaish";
+console.log(name.toUpperCase());
+let store= name.toUpperCase();
+console.log(store);
+console.log(store.toLowerCase());
+store2=store.toLowerCase();
+console.log(store2);
+console.log("------------------------------"); // shows the original string is unchanged because of immutability of js.
