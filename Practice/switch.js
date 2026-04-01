100e7f07f4c8c5105bf0e8fac8bb25e8832ae273
Yash Vaish <yashvx484@gmail.com>
2026-04-02 01:26:09 +0530

    problem 3

---
+++ Practice/switch.js
@@ -0,0 +1,42 @@
+let signal="green";
+ switch (signal){
+ case "red":
+    console.log(" 2nd Humans with civic sense will stop");
+    break;
+ case "yellow":
+    console.log(" 2nd Humans with brain get ready, some without doesnt stops");
+    break;
+case "green":
+    console.log("2nd Go Go Gooo before it turns red againnnn!!");
+    break; 
+  deault:
+  console.log("CHALAAN KATEGA AB");
+ }      
+
+let weekday= 7;
+switch (weekday){
+   case 1:
+      console.log("Monday Today");
+      break;
+   case 2:
+      console.log("Tuesday Today");
+      break;
+   case 3:
+      console.log("Wednesday Today");
+      break;
+   case 4:
+      console.log("Thursday Today");
+      break;            
+   case 5:
+      console.log("Friday Today");
+      break;
+   case 6:
+      console.log("Saturday Today");
+      break;
+   case 7:
+      console.log("2nd Sunday Today");
+      break;   
+      default:
+         console.log("stop your dry days asap");
+         
+}
